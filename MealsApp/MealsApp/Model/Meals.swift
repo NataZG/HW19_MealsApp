55438043de62b96304b011803e16d0e353f59f70
NataZG <86071128+NataZG@users.noreply.github.com>
2021-07-15 21:46:44 +0300

    UI elements created

--- MealsApp/MealsApp/Model/Meals.swift
+++ MealsApp/MealsApp/Model/Meals.swift
@@ -5,4 +5,39 @@
 //  Created by Nata on 15.07.2021.
 //
 
-import Foundation
+import UIKit
+
+struct Meal {
+    
+    let name: String
+    let image: UIImage
+    let notes: String
+    
+    var rating: Int {
+        var retingSum = 0
+        feedbacks.forEach { feedback in
+            retingSum += feedback.rating
+        }
+        return retingSum / feedbacks.count
+    }
+    
+    var feedbacks: [Feedback]
+
+    var ratingBar: String {
+        String(repeating: "⭐️", count: rating) // control + command + " "
+    }
+}
+
+struct Feedback {
+    let feedback: String
+    let date: Date = Date()
+    let rating: Int
+    
+    var currentDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        return dateFormatter.string(from: date)
+    }
+}
