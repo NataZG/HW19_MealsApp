b8633e343d7e976a96359d48b46612a5ebcdd586
NataZG <86071128+NataZG@users.noreply.github.com>
2021-07-15 21:08:29 +0300

    Project Created

---
+++ MealsApp/MealsApp/Controllers/MealDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MealDetailViewController.swift
+//  MealsApp
+//
+//  Created by Nata on 15.07.2021.
+//
+
+import UIKit
+
+class MealDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MealsApp/MealsApp/Model/Meals.swift
@@ -0,0 +1,8 @@
+//
+//  Meals.swift
+//  MealsApp
+//
+//  Created by Nata on 15.07.2021.
+//
+
+import Foundation
